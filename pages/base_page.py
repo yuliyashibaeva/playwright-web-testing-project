64e1ad32b1f2fe4908f48a0fd3e5c8674b220478
yuliya.shibaeva <yuliya.shibaeva.ys@gmail.com>
2024-08-04 14:50:13 +0300

    added comment

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Playwright, Page
 class BasePage:
     def __init__(self, page: Page, playwright: Playwright):
         self.page = page
-        playwright.selectors.set_test_id_attribute("data-test")
+        playwright.selectors.set_test_id_attribute("data-test")  # needed to use custom test-id locator
 
     def open(self, link):
         self.page.goto(link)
