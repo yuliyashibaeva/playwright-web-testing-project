2132b5e83ab2589f88de98cb56b87d162f6d4025
yuliya.shibaeva <yuliya.shibaeva.ys@gmail.com>
2024-08-18 20:48:33 +0300

    added validation checks for checkout inputs

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -3,10 +3,19 @@ from playwright.sync_api import expect
 
 
 class CheckoutPage(BasePage):
-    def fill_customer_info(self, first_name, last_name, postal_code):
+    def fill_customer_info(self, first_name, last_name, postal_code, missing_input: str = None):
         self.page.get_by_test_id("firstName").fill(first_name)
         self.page.get_by_test_id("lastName").fill(last_name)
         self.page.get_by_test_id("postalCode").fill(postal_code)
+
+        if missing_input is None:
+            pass
+        elif missing_input == "First Name":
+            self.page.get_by_test_id("firstName").clear()
+        elif missing_input == "Last Name":
+            self.page.get_by_test_id("lastName").clear()
+        elif missing_input == "Postal Code":
+            self.page.get_by_test_id("postalCode").clear()
 
     def click_continue_button(self):
         self.page.get_by_test_id("continue").click()
@@ -16,4 +25,6 @@ class CheckoutPage(BasePage):
 
     def checkout_complete_text_should_be_present(self):
         expect(self.page.get_by_test_id("complete-text")).to_be_visible()
-        
+
+    def error_message_text_should_be_consistent(self, missing_input):
+        expect(self.page.get_by_test_id("error")).to_have_text(f"Error: {missing_input} is required")
---
+++ tests/test_checkout_form.py
@@ -0,0 +1,19 @@
+from test_data.user_credentials import STANDARD_USER_USERNAME, PASSWORD
+import pytest
+
+
+@pytest.mark.checkout_inputs_validation
+@pytest.mark.parametrize("missing_input", ["First Name", "Last Name", "Postal Code"])
+class TestCheckoutForm:
+    def test_checkout_should_fail_without_required_field(self, login_page, product_list_page, cart_page, checkout_page,
+                                                         customer_data, missing_input):
+        login_page.open_login_page()
+        login_page.login(STANDARD_USER_USERNAME, PASSWORD)
+        product_list_page.add_bolt_t_shirt_to_cart()
+        product_list_page.go_to_cart()
+        cart_page.go_to_checkout()
+        checkout_page.fill_customer_info(customer_data.first_name, customer_data.last_name, customer_data.postal_code,
+                                         missing_input)
+        checkout_page.click_continue_button()
+        checkout_page.error_message_should_be_present()
+        checkout_page.error_message_text_should_be_consistent(missing_input)
